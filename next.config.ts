55a77d16b77ee5db9549ccb98e09336e29fe9918
keislmax <samsonk@lmax.com>
2026-05-28 16:34:37 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [];
+  },
+  env: {
+    NEXT_PUBLIC_VERCEL_URL: process.env.VERCEL_URL || '',
+  },
 };
 
 export default nextConfig;
